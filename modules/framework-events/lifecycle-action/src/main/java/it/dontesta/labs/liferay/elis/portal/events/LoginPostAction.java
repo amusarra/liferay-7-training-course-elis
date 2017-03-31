a824fbd6c38f8121943dc2bc9d7aaf35d367add1
amusarra <antonio.musarra@gmail.com>
2017-03-31 11:24:40 +0200

    Remove code for LPS-70492 - https://issues.liferay.com/browse/LPS-70492

--- modules/framework-events/lifecycle-action/src/main/java/it/dontesta/labs/liferay/elis/portal/events/LoginPostAction.java
+++ modules/framework-events/lifecycle-action/src/main/java/it/dontesta/labs/liferay/elis/portal/events/LoginPostAction.java
@@ -4,30 +4,44 @@
 
 package it.dontesta.labs.liferay.elis.portal.events;
 
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.List;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.osgi.service.component.annotations.ReferencePolicy;
 
-import com.liferay.message.boards.kernel.service.MBMessageServiceUtil;
+import com.liferay.message.boards.kernel.model.MBMessageConstants;
+import com.liferay.message.boards.kernel.service.MBMessageService;
 import com.liferay.portal.kernel.events.ActionException;
 import com.liferay.portal.kernel.events.LifecycleAction;
 import com.liferay.portal.kernel.events.LifecycleEvent;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.Validator;
 
 /**
  * @author Antonio Musarra
  */
-@Component(immediate = true, property = {
-	"key=" + PropsKeys.LOGIN_EVENTS_POST
-}, service = LifecycleAction.class)
+@Component(
+	immediate = true, 
+	property = {
+		"key=" + PropsKeys.LOGIN_EVENTS_POST
+	}, 
+	service = LifecycleAction.class
+)
 public class LoginPostAction implements LifecycleAction {
 
 	@Override
@@ -43,16 +57,32 @@ public class LoginPostAction implements LifecycleAction {
 
 			if (_log.isInfoEnabled()) {
 				_log.info("UserId:" + user.getUserId());
-				_log.info("Full Name:" + user.getFullName());
 			}
 			
-			//PermissionThreadLocal.getPermissionChecker().
+			PrincipalThreadLocal.setName(user.getUserId());
+			PermissionChecker permissionChecker = PermissionCheckerFactoryUtil.create(user);
+			PermissionThreadLocal.setPermissionChecker(permissionChecker);			
+			PermissionThreadLocal.getPermissionChecker();
 			
-			_messageServiceUtil.addMessage(
-				30875, "Messaggio di benvenuto",
-				"Ciao " + user.getFullName() + "benvenuto sul mio portale :-)",
-				ServiceContextThreadLocal.getServiceContext());
-		}
+			ServiceContext serviceContext = ServiceContextThreadLocal.getServiceContext();
+			
+			if (Validator.isNull(serviceContext)) {
+				serviceContext = new ServiceContext();
+			}
+			
+			serviceContext.setUserId(user.getUserId());
+			serviceContext.setScopeGroupId(user.getGroupId());
+			
+			// #LPS-70492 - https://issues.liferay.com/browse/LPS-70492
+			List<ObjectValuePair<String, InputStream>> inputStreamOVPs =
+							Collections.emptyList();
+
+/*			_messageService.addMessage(
+				20147l, 30875l, "Messaggio di benvenuto",
+				"Ciao benvenuto sul mio portale :-)",
+				MBMessageConstants.DEFAULT_FORMAT, inputStreamOVPs, false, 0.0,
+				false, serviceContext);
+*/		}
 		catch (Exception e) {
 			throw new ActionException(e);
 		}
@@ -65,18 +95,19 @@ public class LoginPostAction implements LifecycleAction {
 	}
 
 	@Reference(policy = ReferencePolicy.DYNAMIC, cardinality = ReferenceCardinality.OPTIONAL)
-	protected void setMBMessageServiceUtil(
-		MBMessageServiceUtil mbMessageServiceUtil) {
-		_messageServiceUtil = mbMessageServiceUtil;
+	protected void setMBMessageService(
+		MBMessageService mbMessageService) {
+		_messageService = mbMessageService;
 	}
 
-	protected void unsetMBMessageServiceUtil(
-		MBMessageServiceUtil mbMessageServiceUtil) {
-		_messageServiceUtil = null;
+	protected void unsetMBMessageService(
+		MBMessageService mbMessageServiceUtil) {
+		_messageService = null;
 	}
 
-	private static MBMessageServiceUtil _messageServiceUtil;
+	private MBMessageService _messageService;
 	private Portal _portal;
+	
 	private static final Log _log =
 		LogFactoryUtil.getLog(LoginPostAction.class);
 
